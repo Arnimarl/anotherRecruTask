09a43202534d16b3a0f75ed65f8ec42ef51ea2b2
Lukas <lukaszslomsk13@gmail.com>
2019-10-27 23:17:42 +0100

    formatting changes

--- src/main.js
+++ src/main.js
@@ -1,42 +1,36 @@
 import './styles/main.css';
-require('babel-core/register');
-require('babel-polyfill');
+require("babel-core/register");
+require("babel-polyfill");
 
 const newsModule = (function() {
-  const guardianAPIUrl = 'https://content.guardianapis.com/search?';
-  const guardianAPIKey = 'b9d2f34a-7913-411a-ab27-bb1cb526e304';
+  const guardianAPIUrl = "https://content.guardianapis.com/search?";
+  const guardianAPIKey = "b9d2f34a-7913-411a-ab27-bb1cb526e304";
   const maxNumOfPagesForPaginaton = 50;
-  const paginationSelectEl = document.getElementById('activePageSelect');
-  const sectionSelectEl = document.getElementById('sectionSelect');
+  const paginationSelectEl = document.getElementById("activePageSelect");
+  const sectionSelectEl = document.getElementById("sectionSelect");
 
   function getNews(params) {
-    let sectionQuery = '';
+    let sectionQuery = "";
     const sectionName = sectionSelectEl.value;
 
     if (!params.page) {
       params.page = 1;
     }
 
-    if (sectionName !== 'all') {
+    if (sectionName !== "all") {
       sectionQuery = `&section=${sectionName}`;
     }
 
-    const url = `${guardianAPIUrl}from-date=2019-09-27&page=${
-      params.page
-    }${sectionQuery}&api-key=${guardianAPIKey}`;
+    const url = `${guardianAPIUrl}from-date=2019-09-27&page=${params.page}${sectionQuery}&api-key=${guardianAPIKey}`;
 
     return fetch(url)
-      .then(res => {
-        return res.json();
-      })
-      .then(json => {
-        return json.response;
-      });
+      .then(res => {return res.json()})
+      .then(json => {return json.response});
   }
 
   function adjustPagination(pages) {
-    let paginationEl = document.getElementById('activePageSelect');
-    paginationEl.innerHTML = '';
+    let paginationEl = document.getElementById("activePageSelect");
+    paginationEl.innerHTML = "";
 
     if (pages > maxNumOfPagesForPaginaton) {
       pages = maxNumOfPagesForPaginaton;
@@ -45,27 +39,23 @@ const newsModule = (function() {
     for (let i = 1; i <= pages; i++) {
       paginationEl.innerHTML += `<option value="${i}">${i}</option>`;
     }
+    
   }
 
   function renderNews(news) {
-    const newsList = document.querySelector('.newsList');
-    const newsElTemplate = document
-      .querySelectorAll('.singleNews')[0]
-      .cloneNode(true);
+    const newsList = document.querySelector(".newsList");
+    const newsElTemplate = document.querySelectorAll(".singleNews")[0].cloneNode(true);
 
-    newsList.innerHTML = '';
+    newsList.innerHTML = "";
 
     news.results.forEach(singleNews => {
       const newsEl = newsElTemplate.cloneNode(true);
       const pubDate = new Date(singleNews.webPublicationDate);
 
-      newsEl.querySelector('.newsTitle').innerHTML = singleNews.webTitle;
-      newsEl.querySelector('.newsSectionName').innerHTML =
-        singleNews.sectionName;
-      newsEl.querySelector(
-        '.newsPublicationDate'
-      ).innerHTML = `${pubDate.getDate()}.${pubDate.getMonth()}.${pubDate.getFullYear()}`;
-      newsEl.querySelector('.newsLink').setAttribute('href', singleNews.webUrl);
+      newsEl.querySelector(".newsTitle").innerHTML = singleNews.webTitle;
+      newsEl.querySelector(".newsSectionName").innerHTML = singleNews.sectionName;
+      newsEl.querySelector(".newsPublicationDate").innerHTML = `${pubDate.getDate()}.${pubDate.getMonth()}.${pubDate.getFullYear()}`;
+      newsEl.querySelector(".newsLink").setAttribute("href", singleNews.webUrl);
       newsList.appendChild(newsEl);
     });
   }
@@ -81,26 +71,23 @@ const newsModule = (function() {
   }
 
   function initListeners() {
-    paginationSelectEl.addEventListener('change', () => {
-      displayLastNews({
-        page: paginationSelectEl.value,
-        doNotAdjustPagination: true,
-      });
+    paginationSelectEl.addEventListener("change", () => {
+      displayLastNews({page: paginationSelectEl.value, doNotAdjustPagination: true});
     });
 
-    sectionSelectEl.addEventListener('change', () => {
+    sectionSelectEl.addEventListener("change", () => {
       displayLastNews();
     });
   }
-
+  
   function init() {
     displayLastNews();
     initListeners();
   }
 
   return {
-    init,
-  };
+    init
+  }
 })();
 
 newsModule.init();
